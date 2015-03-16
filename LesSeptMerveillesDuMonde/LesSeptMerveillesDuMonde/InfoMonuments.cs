558ad586bd1ca8d30b50d1e13e18b5db90a799c8
Thomas Burgio <thomas.burgio@y-nov.com>
2015-03-16 11:53:32 +0100

    Update

---
+++ LesSeptMerveillesDuMonde/LesSeptMerveillesDuMonde/InfoMonuments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesSeptMerveillesDuMonde
+{
+    class InfoMonuments
+    {
+        public int pageid { get; set; }
+        public int ns { get; set; }
+        public string title { get; set; }
+        public Object[] revisions { get; set; }
+    }
+
+
+}
--- LesSeptMerveillesDuMonde/LesSeptMerveillesDuMonde/MainPage.xaml.cs
+++ LesSeptMerveillesDuMonde/LesSeptMerveillesDuMonde/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.IO;
 using System.Text;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
 
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -20,17 +21,11 @@ using Windows.Devices.Geolocation;
 
 using Bing.Maps;
 using Bing.Maps.Search;
-using System.Diagnostics;
+using Newtonsoft.Json;
 
-
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace LesSeptMerveillesDuMonde
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
@@ -42,53 +37,68 @@ namespace LesSeptMerveillesDuMonde
         {   //Muraille
             myMap.Center = new Location(40.440023, 117.257423); // Centre la carte au coord.
             myMap.ZoomLevel = 18;
+
+            convertUrl("La%20grande%20muraille%20de%20Chine");
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {   // Pétra
             myMap.Center = new Location(30.3251734, 35.4425807);
             myMap.ZoomLevel = 18;
+
+            convertUrl("Petra");
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {   // Christ
             myMap.Center = new Location(-22.952703, -43.211299);
             myMap.ZoomLevel = 18;
+
+            convertUrl("Christ%20redempteur");
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {   // Machu picchu
             myMap.Center = new Location(-13.1631412, -72.5449628);
             myMap.ZoomLevel = 18;
+
+            convertUrl("Machu%20Picchu");
         }
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {   // Chichen itza
             myMap.Center = new Location(20.6826180, -88.5686445);
             myMap.ZoomLevel = 18;
+
+            convertUrl("Chichen%20Itza");
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {   //colisée
             myMap.Center = new Location(41.8902102, 12.4922308);
             myMap.ZoomLevel = 19;
+
+            convertUrl("Colisée");
         }
 
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {   // Taj Mahal
             myMap.Center = new Location(27.173891, 78.042068);
             myMap.ZoomLevel = 18;
+
+            convertUrl("Taj%20Mahal");
         }
 
         private void Button_Click_8(object sender, RoutedEventArgs e)
         {   // Pyramide
             myMap.Center = new Location(29.9757396, 31.1320991);
             myMap.ZoomLevel = 17;
+
+            convertUrl("Les%20pyramide%20de%20Kheops");
         }
 
         private async void Button_Click_9(object sender, RoutedEventArgs e)
         {   // Search monuments
-
             string locate = searchBox.Text;
 
             // Set the address string to geocode
@@ -99,7 +109,33 @@ namespace LesSeptMerveillesDuMonde
             Bing.Maps.Search.LocationDataResponse response = await searchManager.GeocodeAsync(requestOptions);
 
             myMap.Center = new Location(response.LocationData[0].Location);
-            myMap.ZoomLevel = 15;
+            myMap.ZoomLevel = 16;
+        }
+
+        private async void convertUrl(string monuments)
+        {
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create("http://fr.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&rvsection=0&rvparse&titles=" + monuments);
+            using (HttpWebResponse response = (HttpWebResponse)(await myRequest.GetResponseAsync()))
+            {
+                string ResponseText;
+                string[] ResponseArray;
+                string test;
+                string tests;
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    ResponseText = reader.ReadToEnd();
+                    ResponseArray = ResponseText.Split(new string[] { "\"pageid\"" }, StringSplitOptions.None);
+                    test = "{ \"pageid\"" + ResponseArray[1];
+                    tests = test.Replace("}]}}}}", "}]}");
+
+                   /* Debug.WriteLine(string.Join(", ", ResponseText));
+                    Debug.WriteLine(string.Join(", ", tests));*/
+                    InfoMonuments objJson = new InfoMonuments();
+
+                    objJson = JsonConvert.DeserializeObject<InfoMonuments>(tests);
+
+                }
+            }
         }
     }
 }
