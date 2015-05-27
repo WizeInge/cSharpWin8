c76129ff67d45565555a8900afdcce2584aa10f4
Thomas Burgio <thomas.burgio@y-nov.com>
2015-05-27 14:31:56 +0200

    Last opti

--- LesSeptMerveillesDuMonde/LesSeptMerveillesDuMonde/MainPage.xaml.cs
+++ LesSeptMerveillesDuMonde/LesSeptMerveillesDuMonde/MainPage.xaml.cs
@@ -34,7 +34,7 @@ namespace LesSeptMerveillesDuMonde
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {   //Muraille
-            myMap.Center = new Location(40.440023, 117.257423); // Centre la carte au coord.
+            myMap.Center = new Location(40.440796, 117.260170); // Centre la carte au coord.
             myMap.ZoomLevel = 18;
 
             Uri muraille = new Uri("http://fr.wikipedia.org/wiki/Grande_Muraille");
@@ -43,7 +43,7 @@ namespace LesSeptMerveillesDuMonde
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {   // Pétra
-            myMap.Center = new Location(30.3251734, 35.4425807);
+            myMap.Center = new Location(30.327193, 35.444617);
             myMap.ZoomLevel = 18;
 
             Uri petra = new Uri("http://fr.wikipedia.org/wiki/P%C3%A9tra");
@@ -52,7 +52,7 @@ namespace LesSeptMerveillesDuMonde
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {   // Christ
-            myMap.Center = new Location(-22.952703, -43.211299);
+            myMap.Center = new Location(-22.952676, -43.209581);
             myMap.ZoomLevel = 18;
 
             Uri christ = new Uri("http://fr.wikipedia.org/wiki/Christ_R%C3%A9dempteur");
@@ -61,7 +61,7 @@ namespace LesSeptMerveillesDuMonde
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {   // Machu picchu
-            myMap.Center = new Location(-13.1631412, -72.5449628);
+            myMap.Center = new Location(-13.163065, -72.543758);
             myMap.ZoomLevel = 18;
 
             Uri machu = new Uri("http://fr.wikipedia.org/wiki/Machu_Picchu");
@@ -70,7 +70,7 @@ namespace LesSeptMerveillesDuMonde
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {   // Chichen itza
-            myMap.Center = new Location(20.6826180, -88.5686445);
+            myMap.Center = new Location(20.682929, -88.567216);
             myMap.ZoomLevel = 18;
 
             Uri itza = new Uri("http://fr.wikipedia.org/wiki/Chich%C3%A9n_Itz%C3%A1");
@@ -79,7 +79,7 @@ namespace LesSeptMerveillesDuMonde
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {   //colisée
-            myMap.Center = new Location(41.8902102, 12.4922308);
+            myMap.Center = new Location(41.890256, 12.493429);
             myMap.ZoomLevel = 19;
 
             Uri colisee = new Uri("http://fr.wikipedia.org/wiki/Colis%C3%A9e");
@@ -88,7 +88,7 @@ namespace LesSeptMerveillesDuMonde
 
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {   // Taj Mahal
-            myMap.Center = new Location(27.173891, 78.042068);
+            myMap.Center = new Location(27.173204, 78.044039);
             myMap.ZoomLevel = 18;
 
             Uri TajMhal = new Uri("http://fr.wikipedia.org/wiki/Taj_Mahal");
@@ -97,7 +97,7 @@ namespace LesSeptMerveillesDuMonde
 
         private void Button_Click_8(object sender, RoutedEventArgs e)
         {   // Pyramide
-            myMap.Center = new Location(29.9757396, 31.1320991);
+            myMap.Center = new Location(29.975694, 31.136204);
             myMap.ZoomLevel = 17;
 
             Uri pyramide = new Uri("http://fr.wikipedia.org/wiki/Pyramide_de_Kh%C3%A9ops");
@@ -116,12 +116,15 @@ namespace LesSeptMerveillesDuMonde
                 // Make the geocode request 
                 Bing.Maps.Search.SearchManager searchManager = myMap.SearchManager;
                 Bing.Maps.Search.LocationDataResponse response = await searchManager.GeocodeAsync(requestOptions);
+                if (response != null)
+                {
+                    myMap.Center = new Location(response.LocationData[0].Location);
+                    myMap.ZoomLevel = 16;
 
-                myMap.Center = new Location(response.LocationData[0].Location);
-                myMap.ZoomLevel = 16;
-
-                //Mise a 0 de l'opacité du webview
-                WebContainer.Opacity = 0;
+                    //Mise a 0 de l'opacité du webview
+                    WebContainer.Opacity = 0;
+                }
+               
             }
             else
             {
@@ -131,7 +134,7 @@ namespace LesSeptMerveillesDuMonde
 
         private void displayHtml(Uri pageHtml)
         {
-            //Affiche la webView avec la page html correspondate !
+            //Affiche la webView avec la page html correspondante !
             WebContainer.Opacity = 0.5;
             WebContainer.Navigate(pageHtml);
         }
